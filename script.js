1e4e5391a8f6b22db7742c062ba1166923c8f05d
Vanessa Furquim <vanessafurquim.contato@gmail.com>
2022-04-18 18:00:37 -0300

    commit entrega

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let userName;
+const initialScreen = document.querySelector(".initial-screen");
+const mainScreen = document.querySelector(".chat-container");
 
-requestUserName ()
+requestUserName();
+sendMessagesWithEnterKey();
 
-function requestUserName () {
+function requestUserName() {
 
-    userName = prompt ("Qual seu nome?");
+    mainScreen.classList.add("hidden");
+
+    userName = document.querySelector(".enter-user-name").value;
 
     const responseUserName = axios.post('https://mock-api.driven.com.br/api/v4/uol/participants', {name: userName});
 
@@ -12,27 +17,37 @@ function requestUserName () {
     responseUserName.catch(invalidUserName);
 }
 
-function successfulResponse () {
+function successfulResponse() {
+
+    onlineStatus();
+    receiveMessages();
+
     setInterval(receiveMessages, 3000);
     setInterval(onlineStatus, 5000);
 }
 
-function invalidUserName () {
+function invalidUserName() {
     alert ("Nome de usuário indisponível. Por favor, escolha outro.");
-    requestUserName;
+    requestUserName();
 }
 
-function onlineStatus () {
+function onlineStatus() {
     axios.post('https://mock-api.driven.com.br/api/v4/uol/status', {name: userName});
 }
 
-function receiveMessages () {
+function receiveMessages() {
+
+    initialScreen.classList.add("hidden");
+    mainScreen.classList.remove("hidden");
+
     const receivedMessages = axios.get('https://mock-api.driven.com.br/api/v4/uol/messages');
     receivedMessages.then(exhibitMessagesOnScreen);
 }
 
-function exhibitMessagesOnScreen (receivedMessages) {
+function exhibitMessagesOnScreen(receivedMessages) {
+
     const addMessage = document.querySelector(".chat-container");
+
     addMessage.innerHTML = "";
 
     for(let i = 0; i < receivedMessages.data.length; i++){
@@ -42,12 +57,12 @@ function exhibitMessagesOnScreen (receivedMessages) {
             `<span class="single-message status" data-identifier="message">
                 <p class="sent-time">(${receivedMessages.data[i].time})</p>&nbsp;<strong>${receivedMessages.data[i].from}</strong>&nbsp;para&nbsp;<strong>${receivedMessages.data[i].to}</strong>:&nbsp;<div class="message-text">${receivedMessages.data[i].text}</div>
             </span>`
-        } else if (receivedMessages.data[i].type === "message") {
+        } else if(receivedMessages.data[i].type === "message") {
             addMessage.innerHTML += 
             `<span class="single-message message" data-identifier="message">
                 <p class="sent-time"> (${receivedMessages.data[i].time})</p>&nbsp;<strong>${receivedMessages.data[i].from}</strong>&nbsp;para&nbsp;<strong>${receivedMessages.data[i].to}</strong>:&nbsp;${receivedMessages.data[i].text}
             </span>`
-        } else if (receivedMessages.data[i].type === "private_message" && receivedMessages.data[i].to === userName) {
+        } else if(receivedMessages.data[i].type === "private_message" && receivedMessages.data[i].to === userName) {
             addMessage.innerHTML +=
             `<span class="single-message private-message" data-identifier="message">
                 <p class="sent-time"> (${receivedMessages.data[i].time})</p>&nbsp;<strong>${receivedMessages.data[i].from}</strong>&nbsp;para <strong>${receivedMessages.data[i].to}</strong>:&nbsp;${receivedMessages.data[i].text}
@@ -59,7 +74,9 @@ function exhibitMessagesOnScreen (receivedMessages) {
     scrollToLastMessage.scrollIntoView();
 }
 
-function sendMessages () {
+function sendMessages() {
+
+
     let usersMessages = document.querySelector(".user-message").value;
     let usersSingleMessage = axios.post('https://mock-api.driven.com.br/api/v4/uol/messages',
     {from: userName,
@@ -69,4 +86,16 @@ function sendMessages () {
 
      usersSingleMessage.then(successfulResponse);
      usersSingleMessage.catch(window.location.reload);
+}
+
+function sendMessagesWithEnterKey() {
+
+    const enterButtonActive = document.getElementById("activate-enter-key");
+
+    enterButtonActive.addEventListener("keyup", function(event) {
+        if(event.keyCode === 13) {
+        event.preventDefault();
+        document.getElementById("send-message-with-enter-key").click();
+        }
+    });
 }
